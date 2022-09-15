9c83a0196bc8f0bbbff5e02620352ac293b21ebe
Wahidhidayatulla <wahidhidayatullah770@gmail.com>
2022-09-16 02:23:28 +0700

    fix-bot-v2

---
+++ plugins/sound6.js
@@ -0,0 +1,20 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+    let info = fs.readFileSync('./mp3/sound6.mp3')
+
+let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+
+conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow",
+    mediaType: 2,
+    description: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow", 
+    title: 'whmods-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sig  }}})
+}
+handler.command = ['sound6']
+handler.help = ['sound6']
+
+export default handler
---
+++ plugins/sound98.js
@@ -0,0 +1,20 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+    let info = fs.readFileSync('./mp3/sound98.mp3')
+
+let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+
+conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow",
+    mediaType: 2,
+    description: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow", 
+    title: 'whmods-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sig  }}})
+}
+handler.command = ['sound98']
+handler.help = ['sound98']
+
+export default handler
